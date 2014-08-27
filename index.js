74df469d1df71d493c2fd557dc30a4f95292e022
truotaku <najee.s.gardner@gmail.com>
2014-08-27 01:07:40 -0400

    added slight persistance of messages and auto scroll

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ io.on('connection', function(socket) {
 });
 
 
-http.listen(process.env.PORT, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
